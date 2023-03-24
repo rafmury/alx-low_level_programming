6ccb6be2a2e4435ff5468454f1b8ab49e3eeca65
Your Name <raphaelmurimi469@gmail.com>
2023-03-24 15:55:57 -0700

    Task 1.isdigit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,14 +2,14 @@
 
 /**
  * _isdigit - Checks for a digit
- * @x: The number to be checked
+ * @c: The number to be checked
  * Return: 1 for a digit or 0 for otherwise
  */
 
-int _isdigit(int x)
+int _isdigit(int c)
 
 {
-	if (x >= 40 && x <= 57)
+	if (c >= 40 && c <= 57)
 	{
 	return (1);
 	}
