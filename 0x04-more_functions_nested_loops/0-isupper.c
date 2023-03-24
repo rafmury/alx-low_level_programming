e12dd9a4b05a1c121b6830cca8e785ea3d8c6632
Your Name <raphaelmurimi469@gmail.com>
2023-03-24 15:28:48 -0700

    Task 0.isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - Checks for uppercase character
+ * @n: The number to be checked
+ * Return: 1 for uppercase character or 0 otherwise
+ */
+
+int _isupper(int n)
+{
+	if (n >= 65 && n <= 90)
+	{
+	return (1);
+	}
+	return (0);
+}
