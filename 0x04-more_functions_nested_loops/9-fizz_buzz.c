ddf23f6f8d719c99602e37355101f51b648da6d6
Your Name <raphaelmurimi469@gmail.com>
2023-03-24 18:49:51 -0700

    Task 9. Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * main - prints FizzBuzz
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else
+		{
+			printf("%i", i);
+		}
+
+		if (i < 100)
+		{
+			printf(" ");
+		}
+		else
+		{
+			printf("\n");
+		}
+	}
+	return (0);
+}
