378576697921a7f273b5fee785de44340b048939
Your Name <raphaelmurimi469@gmail.com>
2023-03-24 18:21:34 -0700

    Task 7. I feel like I am diagonally parked in a parallel universe

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_diagonal - Prints diagonal
+ *@n: number of spaces
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	int x, y;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (x = 1; x <= n; x++)
+		{
+			if (x > 1)
+			{
+				for (y = 1; y <= x - 1; y++)
+				{
+				_putchar(' ');
+				}
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}
