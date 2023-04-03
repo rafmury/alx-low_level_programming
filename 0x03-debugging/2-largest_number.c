dac3e1f6494ef9e5e733b9c48e00b49522b7e56c
Your Name <raphaelmurimi469@gmail.com>
2023-04-03 03:17:13 -0700

    Task 2. 0 > 972?

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -16,7 +16,7 @@ int largest_number(int a, int b, int c)
 	{
 		largest = a;
 	}
-	else if (a >c && c > b)
+	else if (a > c && c > b)
 	{
 		largest = a;
 	}
@@ -29,10 +29,6 @@ int largest_number(int a, int b, int c)
 		largest = b;
 	}
 	else if (c > a && a > b)
-	{
-		largest = c;
-	}
-	else if (c > b && b > a)
 	{
 		largest = c;
 	}
