f36ad73c1815ba14b055a7bdd53812082d8e1562
Your Name <raphaelmurimi469@gmail.com>
2023-04-10 16:43:53 -0700

    Task 0. A library is not a luxury but one of the necessities of life

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isdigit - Checks for a digit
+ * @c: The number to be checked
+ * Return: 1 for a digit or 0 for otherwise
+ */
+
+int _isdigit(int c)
+
+{
+	if (c >= 48 && c <= 57)
+	{
+	return (1);
+	}
+	return (0);
+}
